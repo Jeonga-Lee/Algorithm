04a709535688979160f78877c5d3635dfef9eedf
Jeonga-Lee <201424077b@gmail.com>
2023-07-31 10:01:59 +0900

    배열 뒤집기

--- baekjoon_10811/src/baekjoon_10811/Main.java
+++ baekjoon_10811/src/baekjoon_10811/Main.java
@@ -7,11 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main{
 	public static void main (String args[]) throws IOException{
-<<<<<<< HEAD
-		 
-=======
 		
->>>>>>> refs/remotes/origin/master
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
